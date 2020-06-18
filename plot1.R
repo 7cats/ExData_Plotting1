e6b1f3b4fe66217c1f1b8a6f56e5471a5847ed44
Batata <Zhangzixin.dalian@gmail.com>
2020-06-18 20:07:06 +0200

    plot1.R created

---
+++ plot1.R
@@ -0,0 +1,9 @@
+#####   plot1 code  #####
+
+### Reading data
+data <- read.table("household_power_consumption.txt",header = F, sep = ";" ,
+                   skip = grep("1/2/2007", readLines("household_power_consumption.txt"))[1]-1,
+                   nrows = grep("3/2/2007", readLines("household_power_consumption.txt"))[1] 
+                             - grep("1/2/2007", readLines("household_power_consumption.txt"))[1]
+                   ) # only read lines with specific days
+colnames(data) <- read.table("household_power_consumption.txt",header = F, sep = ";" ,nrow = 1) # setname
